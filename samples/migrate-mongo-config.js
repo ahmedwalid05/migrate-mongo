b92ff5121a3d4bac67b3f850a0203c6d5176f1d2
Ahmed <ahmedwalid05@gmail.com>
2021-11-02 22:29:28 +0300

    Added a custom script runner to allow users to run the script for multiple databases

--- samples/migrate-mongo-config.js
+++ samples/migrate-mongo-config.js
@@ -13,7 +13,7 @@ const config = {
       useUnifiedTopology: true, // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
-    }
+    },
   },
 
   // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
@@ -22,12 +22,16 @@ const config = {
   // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
   changelogCollectionName: "changelog",
 
-  // The file extension to create migrations and search for in migration dir 
+  // The file extension to create migrations and search for in migration dir
   migrationFileExtension: ".js",
+
+  // Allows you to call the up and down function through it with your arguments.
+  // It's recommended to add your own migration-sample.js file to the migrations directory if you change the arguments.
+  // customRunnerPath: "./script.js",
 
   // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determin
   // if the file should be run.  Requires that scripts are coded to be run multiple times.
-  useFileHash: false
+  useFileHash: false,
 };
 
 // Return the config as a promise
